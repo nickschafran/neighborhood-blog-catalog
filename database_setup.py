d0af194f66295e77c8fc56c5df204cd83479731f
nickschafran <nickschafran@fastmail.fm>
2015-08-09 01:20:57 -0400

    added functionality to database_setup.py
    added @property methods

--- database_setup.py
+++ database_setup.py
@@ -15,18 +15,38 @@ class Region(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
 
 class RegionBlog(Base):
     """Blogs focusing on Regions"""
-    __tablename__ = 'region_blog'
+    __tablename__ = 'menu_item'
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    url = Column(String(250))
+    url = Column(String(8))
     region_id = Column(Integer, ForeignKey('region.id'))
     region = relationship(Region)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'url': self.url,
+        }
+
+
 engine = create_engine('sqlite:///regionblogs.db')
+
 
 Base.metadata.create_all(engine)
